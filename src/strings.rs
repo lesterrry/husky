7c7c2b0369f0949c939324e39eb911f898d15a49
Lester Covey <me@lestercovey.ml>
2022-04-06 22:21:43 +0300

    Huge leap imma genius

---
+++ src/strings.rs
@@ -0,0 +1,12 @@
+pub const LOGO: &str = 
+"   __            __       
+  / /  __ _____ / /____ __
+ / _ \\/ // (_-</  '_/ // /
+/_//_/\\_,_/___/_/\\_\\\\_, / 
+                   /___/  ";
+
+pub const USAGE_INSTRUCTIONS: &str = 
+"USAGE INSTRUCTIONS:
+[ARROW UP] / [ARROW DOWN] – Switch between active input blocks
+[ENTER] – Submit input
+[F9] / [CTRL + C] – Exit";
